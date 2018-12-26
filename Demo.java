3d42ac442799ac01e11f7406c945be864aac1934
ziliao0258 <ziliao0258@126.com>
2018-12-27 01:13:57 +0800

    zx

--- Demo.java
+++ Demo.java
@@ -4,7 +4,6 @@ public class Demo{
 		System.out.println("Hello World!!");
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
-		System.out.println("Hello World!!");
 		System.out.println("Hello World!!!");
 	}
 }
